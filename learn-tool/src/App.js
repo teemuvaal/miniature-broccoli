20309fd9942a5770b34eec9b34566c6ace215327
Teemu Vaalasmaa <flipsuk@gmail.com>
2023-05-25 16:59:18 +0300

    Refactored to use 3 components in App, cleaned up code and added comments.

--- learn-tool/src/App.js
+++ learn-tool/src/App.js
@@ -2,17 +2,53 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Header() {
-  return <h1 className="text-center my-4">What do you want to learn?</h1>
+// Handles the header for the site. For now, just text
+const Header = () => {
+  return <h1 className="text-center my-4">What do you want to learn? Tell me.</h1>;
 }
 
-function App() {
-  const [input, setInput] = useState('');
-  const [plan, setPlan] = useState([]);  // New state variable for the learning plan
+// Handles the user input field. Input, setInput & handleSubmit are passed props from App
+const InputField = ({input, setInput, handleSubmit}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+    <input 
+      type="text" 
+      className="form-control"
+      value={input} 
+      onChange={event => setInput(event.target.value)} 
+      placeholder="Type here..."
+    />
+    <button type="submit" className="btn btn-primary mt-3">Submit</button>
+  </form>
+  )
+
+}
+// Handles displaying the plan. Plan is passed as prop from App
+const LearningPlan = ({plan}) => {
+  return (
+    <div className="row">
+    {plan && plan.map((step, index) => (
+      <div key={index} className="col-4">
+        <div className="card my-2">
+          <div className="card-body">
+            <h5 className="card-title">{step.stepName}</h5>
+            <p className="card-text">{step.content}</p>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+  )
+}
+
+const App = () => {  
+//  const [loading, setLoading] = useState(false); // To set up loading animation, need to fix later
+  const [input, setInput] = useState(''); // for user input
+  const [plan, setPlan] = useState([]); // for generating the plan
   const handleSubmit = async event => {
     event.preventDefault();
-
     const apiUrl = 'http://localhost:5000/api/completion';
+    // Prompt that can be modified accordingly and improved. Format needs to be exactly this.
     const data = { prompt: `Create a 5-step learning plan to learn ${input}. Give the plan in an array of objects format with step name and content. Do not include anything but the array of objects itself. Format should be following: {
       "steps": [
         {
@@ -29,47 +65,29 @@ function App() {
     `
     };
     
+//    setLoading(true);
+
     try {
       const response = await axios.post(apiUrl, data);
       
     // Parsing the response string into JSON and then setting the plan state
     const parsedResponse = JSON.parse(response.data.trim());
     console.log(response.data);
-    setPlan(parsedResponse.steps); 
+    setPlan(parsedResponse.steps);
+//    setLoading(false);  
     } catch (error) {
       console.error('Error making API request: ', error);
+//      setLoading(false);
     }
 
     setInput('');
   }
-
   return (
     <div className="container">
       <Header />
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          className="form-control"
-          value={input} 
-          onChange={event => setInput(event.target.value)} 
-          placeholder="Type here..."
-        />
-        <button type="submit" className="btn btn-primary mt-3">Submit</button>
-      </form>
-
+      <InputField setInput={setInput} handleSubmit={handleSubmit} />
+      <LearningPlan plan={plan} />
       {/* Here we render the learning plan */}
-      <div className="row">
-        {plan && plan.map((step, index) => (
-          <div key={index} className="col-4">
-            <div className="card my-2">
-              <div className="card-body">
-                <h5 className="card-title">{step.stepName}</h5>
-                <p className="card-text">{step.content}</p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
     </div>
   );
 }
--- server/server.py
+++ server/server.py
@@ -8,6 +8,8 @@ CORS(app)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
+## Here is endpoint to call OpenAI API. key needs to be in .env file
 @app.route('/api/completion', methods=['POST'])
 @cross_origin()
 def get_completion():
@@ -18,8 +20,9 @@ def get_completion():
       prompt=data['prompt'],
       max_tokens=1024
     )
-
+## Parse the response to a json that FE can read
     return jsonify(completion['choices'][0]['text'].strip())
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
